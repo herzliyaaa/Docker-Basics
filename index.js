343bc99dc3b94ff3484f12abd1ee0c20371d4e67
Herzlia Jane Barangan <herzliajane.barangan@biotechfarms.net>
2022-08-30 16:52:55 +0800

    initialized basic setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require('morgan')
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "My naevis we love you!" });
+});
+
+const PORT = process.env.PORT || 9000;
+
+module.exports = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
